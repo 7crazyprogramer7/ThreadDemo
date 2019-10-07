dfe1a7dfbcd11400c5217f161bbd07054047c3b5
7crazyprogramer7 <3150687874@qq.com>
2019-10-07 18:55:22 +0800

    second commit

--- src/demo_one/FirstThread.java
+++ src/demo_one/FirstThread.java
@@ -2,10 +2,22 @@ package demo_one;
 
 public class FirstThread {
     public static void main(String[] args){
+        Runnable runnble1 = new PrintChar();
+        Runnable runnable2 = new PrintNum();
+
+        Thread thread1 = new Thread(runnble1);
+        Thread thread2 = new Thread(runnable2);
+
+        thread1.start();
+        for (int i =1;i<=100;i++){
+            System.out.println("b");
+        }
+        thread2.start();
+        thread2.setPriority(Thread.MAX_PRIORITY);
 
     }
 
-    class PrintNum implements Runnable{
+    static class PrintNum implements Runnable{
 
         @Override
         public void run() {
@@ -15,7 +27,7 @@ public class FirstThread {
            }
         }
     }
-    class PrintChar implements Runnable{
+    static class PrintChar implements Runnable{
 
         @Override
         public void run() {
